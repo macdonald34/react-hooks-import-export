1a73a1978dfba4f865a3fa5420b1b4df2a4f0a6d
macdonald34 <+>
2024-02-01 23:43:08 +0300

    completed assignment

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,14 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import{trees, wildlife} from "./parks/RockyMountain";
 
+    console.log(trees);
+    wildlife();
+    
 function ColoradoStateParks() {
-  howManyParks(); // => "42 parks!"
-
-  return <h1>Colorado State Parks!</h1>;
+  return(
+    <div>
+      <MesaVerde/>
+    </div>
+  );
 }
